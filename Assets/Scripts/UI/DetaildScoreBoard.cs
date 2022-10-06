9b38800734e636e6c69284c78c753080832813c7
Giuseppe Angelini <giuseppe.angelini@tu-berlin.de>
2022-10-06 10:48:49 +0200

    Score board scrolling bug fixed

--- Assets/Scripts/UI/DetaildScoreBoard.cs
+++ Assets/Scripts/UI/DetaildScoreBoard.cs
@@ -17,6 +17,8 @@ public class DetaildScoreBoard : MonoBehaviour
             lastRow = firstRow;
         }
         GameObject newRow = Instantiate(rowPrefab, lastRow.transform.position, Quaternion.identity, rowsContainer);
+        RectTransform containerRectTransform =  rowsContainer.GetComponent<RectTransform>();
+        containerRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, containerRectTransform.rect.height + newRow.GetComponent<RectTransform>().rect.height);
         RectTransform tmp = lastRow.GetComponent<RectTransform>();
         newRow.GetComponent<RectTransform>().anchoredPosition = new Vector2(tmp.anchoredPosition.x, lastRow.GetComponent<RectTransform>().anchoredPosition.y - newRow.GetComponent<RectTransform>().rect.height);
         lastRow = newRow;
