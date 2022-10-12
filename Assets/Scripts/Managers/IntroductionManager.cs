d9af0f69f40ac1fd580cb7d8e8c9eda51c72a4cc
Giuseppe Angelini <giuseppe.angelini@tu-berlin.de>
2022-10-12 16:26:03 +0200

    Introduction implemented with test text

---
+++ Assets/Scripts/Managers/IntroductionManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class IntroductionManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textBox;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] GameObject endButton;
+    [SerializeField] GameObject transitionPanel;
+    [SerializeField] List<string> introductionTexts;
+
+    int currentText;
+
+    private void Start()
+    {
+        currentText = 0;
+        NextTxt();
+    }
+
+    public void NextTxt()
+    {
+        if (currentText < introductionTexts.Count - 1)
+        {
+            textBox.text = introductionTexts[currentText];
+            currentText++;
+        }
+        else
+        {
+            textBox.text = introductionTexts[currentText];
+            nextButton.SetActive(false);
+            endButton.SetActive(true);
+        }
+    }
+
+    public void Play()
+    {
+        float fadeInTime = transitionPanel.GetComponent<Animator>().runtimeAnimatorController.animationClips[0].length;
+        transitionPanel.SetActive(true);
+        StartCoroutine(NextScene(fadeInTime));
+    }
+
+    private IEnumerator NextScene(float waitingTime)
+    {
+        yield return new WaitForSeconds(waitingTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
