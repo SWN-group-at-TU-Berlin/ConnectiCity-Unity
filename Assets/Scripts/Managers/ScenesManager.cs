03ae00321d3dcfed76c4ef1aaa44be65e0a1a907
Giuseppe Angelini <giuseppe.angelini@tu-berlin.de>
2023-02-06 17:19:18 +0100

    Main Menu implemented

--- Assets/Scripts/Managers/ScenesManager.cs
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -20,10 +20,30 @@ public class ScenesManager : MonoBehaviour
         StartCoroutine(ReloadMainScene(animLenght));
     }
 
+    public void NextScene()
+    {
+        transitionPanel.GetComponent<Animator>().Play("FadeIn", 0, 0f);
+        float animLenght = 0;
+        foreach (AnimationClip anim in transitionPanel.GetComponent<Animator>().runtimeAnimatorController.animationClips)
+        {
+            if (anim.name.Equals("FadeIn"))
+            {
+                animLenght = anim.length;
+            }
+        }
+        StartCoroutine(LoadNextScene(animLenght));
+    }
+
     private IEnumerator ReloadMainScene(float timeToWait)
     {
         yield return new WaitForSeconds(timeToWait);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private IEnumerator LoadNextScene(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
